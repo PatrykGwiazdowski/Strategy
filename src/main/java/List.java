663af1b68f82bb72fe2c423808c2b3097279d75a
jackowskiBLStream <damian.jackowski@blstream.com>
2016-03-04 13:07:06 +0100

    tests added

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -8,43 +8,45 @@ public class List<E> implements ListInterface<E> {
     /**
      * Creates new empty List
      */
-    public List(){
-        head=new Node();
-        size=0;
+    public List() {
+        head = new Node();
+        size = 0;
     }
 
     /**
      * Adds element to end of the List
+     *
      * @param o Element to be added
      */
 
     @Override
     public void addElement(E o) {
-        if(size==0){
-            head.element=o;
-        }else{
+        if (size == 0) {
+            head.element = o;
+        } else {
 
-            Node tmpNode=head;
-            while (tmpNode.nextNode!=null){
-                tmpNode=tmpNode.nextNode;                //interate to end of the list
+            Node tmpNode = head;
+            while (tmpNode.nextNode != null) {
+                tmpNode = tmpNode.nextNode;                //interate to end of the list
             }
-            tmpNode.nextNode=new Node(o,tmpNode,null);
+            tmpNode.nextNode = new Node(o, tmpNode, null);
         }
         size++;
     }
 
     /**
      * Removes element at specified index in List
+     *
      * @param index Index of element to be Removed
      */
     @Override
     public void removeElement(int index) {
-        if(index==0){
+        if (index == 0) {
             removeFirst();
         }
-        if(index>0){
-            Node tmpNode=getNode(index);
-            tmpNode.previousNode.nextNode=tmpNode.nextNode;
+        if (index > 0) {
+            Node tmpNode = getNode(index);
+            tmpNode.previousNode.nextNode = tmpNode.nextNode;
             size--;
         }
     }
@@ -53,10 +55,10 @@ public class List<E> implements ListInterface<E> {
     /**
      * Removes firs element of List
      */
-    public void removeFirst(){
-        Node tmpNode=head;
-        head=tmpNode.nextNode;
-        head.previousNode=null;
+    public void removeFirst() {
+        Node tmpNode = head;
+        head = tmpNode.nextNode;
+        head.previousNode = null;
         size--;
     }
 
@@ -69,17 +71,18 @@ public class List<E> implements ListInterface<E> {
         for(int i=0;i<size;i++){
 
         }*/
-        head=null;
-        size=0;
+        head = null;
+        size = 0;
     }
 
     /**
      * return true if list is empty
+     *
      * @return true  if list is empty
      */
     @Override
     public boolean isEmpty() {
-        if(size==0){
+        if (size == 0) {
             return true;
         }
 
@@ -88,17 +91,18 @@ public class List<E> implements ListInterface<E> {
 
     /**
      * checks if List contains specified elements
+     *
      * @param o List element to check
-     * @return  true if list contains specified element
+     * @return true if list contains specified element
      */
     @Override
     public boolean contains(E o) {
-        Node tmpNode=head;
-        for(int i=0;i<size;i++){
-            if(tmpNode.element.equals(o)){
+        Node tmpNode = head;
+        for (int i = 0; i < size; i++) {
+            if (tmpNode.element.equals(o)) {
                 return true;
             }
-            tmpNode=tmpNode.nextNode;
+            tmpNode = tmpNode.nextNode;
         }
         return false;
     }
@@ -106,30 +110,41 @@ public class List<E> implements ListInterface<E> {
 
     /**
      * returns List Item at specified index
+     *
      * @param index index of element to return
-     * @return  List item at specified index
+     * @return List item at specified index
      */
     @Override
-    public E get(int index) {                                   //TODO something is wrong here
-        return (E) getNode(index).element;                      //List element at specified index
+    public E get(int index) {
+        if (index > size - 1 || index < 0) {
+            return null;
+        } else {
+            return (E) getNode(index).element;                      //List element at specified index
+        }
+
     }
 
     /**
      * replaces value at specified index of list
+     *
      * @param index index of element to replace
-     * @param o Element that will be added to specified index
+     * @param o     Element that will be added to specified index
      */
     @Override
     public void set(int index, E o) {
-        Node tmpNode=head;
-        for(int i=0;i<index;i++){       //iterate to specified node
-            tmpNode=tmpNode.nextNode;
+        if (index <= size - 1 || index >= 0) {
+            Node tmpNode = head;
+            for (int i = 0; i < index; i++) {       //iterate to specified node
+                tmpNode = tmpNode.nextNode;
+            }
+            tmpNode.element = o;                  //switch current List Element with given as parameter
         }
-        tmpNode.element=o;                  //switch current List Element with given as parameter
+
     }
 
     /**
      * returns current size of List
+     *
      * @return size of List
      */
     @Override
@@ -138,15 +153,15 @@ public class List<E> implements ListInterface<E> {
     }
 
     /**
-     *
      * Returns node of list at specified index
+     *
      * @param index index of node
      * @return node at given index of list
      */
     private Node getNode(int index) {
-        Node tmpNode=head;
-        for(int i=0;i<index;i++){           //iterate through all nodes to specified index
-            tmpNode=tmpNode.nextNode;
+        Node tmpNode = head;
+        for (int i = 0; i < index; i++) {           //iterate through all nodes to specified index
+            tmpNode = tmpNode.nextNode;
         }
         return tmpNode;             //node at specified index
     }
--- src/test/java/ListTest.java
+++ src/test/java/ListTest.java
@@ -1,3 +1,5 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,79 +8,177 @@ import static org.junit.Assert.*;
  * Created by INV-6179 on 03.03.2016.
  */
 public class ListTest {
+    private List<Integer> list, listSecond;
+
+    @Before
+    public void setList() {
+        this.list = new List<Integer>();
+        this.listSecond = new List<Integer>();
+        this.listSecond.addElement(0);
+        this.listSecond.addElement(-50);
+        this.listSecond.addElement(10000);
+        this.listSecond.addElement(85723);
+
+    }
+
+    @After
+    public void removeList() {
+        this.listSecond = null;
+        this.list = null;
+    }
+
     @Test
     public void testGetSizeForTwoElements() throws Exception {
-        List<Integer> list=new List();
         list.addElement(1);
         list.addElement(2);
-        assertEquals("Incorrect size after adding element to List",2,list.getSize());
+        assertEquals("Incorrect size after adding element to List", 2, list.getSize());
     }
+
     @Test
-    public void testGetSizeFor10000Elements() throws Exception {
-        List<Integer> list = new List();
+    public void testGetSizeForTenThousandElements() throws Exception {
         for (int i = 0; i < 10000; i++) {
             list.addElement(i);
         }
-        assertEquals("Incorrect list size after adding 100 elements in loop", 100, list.getSize());
+        assertEquals("Incorrect list size after adding 10000 elements in loop", 10000, list.getSize());
     }
+
     @Test
     public void testGetSizeForSameElements() throws Exception {
-        List<Integer> list = new List();
         Integer value = new Integer(2);
         list.addElement(value);
         list.addElement(value);
         assertEquals("incorrect list size after adding 2 same elements", 2, list.getSize());
     }
+
     @Test
-    public void testAddOneElement() throws Exception {
-        List<Integer> list=new List<>();
+    public void testGetSizeForEmptyList() throws Exception {
+        assertEquals("Incorrect list size for empty list", 0, list.getSize());
+    }
+
+    @Test
+    public void testGetSizeAfterAddOneElement() throws Exception {
         list.addElement(1);
-        assertEquals("incorrect size after adding one element",1,list.getSize());
+        assertEquals("incorrect size after adding one element", 1, list.getSize());
     }
+
     @Test
-    public void testAddTwoSame() throws Exception {
-        List<Integer> list=new List<>();
+    public void testGetSizeAfterAddTwoSame() throws Exception {
         list.addElement(1);
         list.addElement(1);
-        assertEquals("incorrect size after adding one element",2,list.getSize());
+        assertEquals("incorrect size after adding one element", 2, list.getSize());
+    }
+
+    @Test
+    public void testGetSizeAfterAddManyElementsAndRemoveOneOfThem() throws Exception {
+        list.addElement(1);
+        list.addElement(5);
+        list.addElement(10000);
+        list.addElement(-5);
+        list.removeElement(2);
+
+        assertEquals("incorrect size after adding many elements and remove one of them", 3, list.getSize());
+    }
+
+    @Test
+    public void testGetSizeAfterRemoveFirst() throws Exception {
+        list.addElement(1);
+        list.addElement(5);
+        list.addElement(100);
+        list.removeFirst();
+        assertEquals("incorrect size after remove first element", 2, list.getSize());
+    }
+
+    @Test
+    public void testGetSizeAfterClearList() throws Exception {
+        list.addElement(1);
+        list.addElement(5);
+        list.addElement(100);
+        list.clear();
+        assertEquals("incorrect size after clear list", 0, list.getSize());
     }
 
 
     @Test
-    public void testRemoveElement() throws Exception {
-
+    public void testIsEmptyWhenHasElements() throws Exception {
+        assertEquals("incorrect status of list", false, listSecond.isEmpty());
     }
 
     @Test
-    public void testRemoveFirst() throws Exception {
-
+    public void testIsEmptyWhenHasNoElements() throws Exception {
+        assertEquals("incorrect status of list", true, list.isEmpty());
     }
 
     @Test
-    public void testClear() throws Exception {
+    public void testIsEmptyAfterAddElementsToEmptyList() throws Exception {
+        list.addElement(5);
+        list.addElement(500);
+        list.addElement(-55);
+        assertEquals("incorrect status of list", false, list.isEmpty());
+    }
 
+    //TODO append if to method below
+   /* @Test
+    public void testIsEmptyAfterRemoveOnlyElement() throws Exception {
+        list.addElement(5);
+        list.removeElement(0);
+        assertEquals("incorrect status of list", true, list.isEmpty());
+    }*/
+    @Test
+    public void testIsEmptyAfterClearList() throws Exception {
+        listSecond.clear();
+        assertEquals("incorrect status of list", true, listSecond.isEmpty());
     }
 
     @Test
-    public void testIsEmpty() throws Exception {
+    public void testContainsWhileHasElement() throws Exception {
 
+        assertEquals("incorrect status of element", true, listSecond.contains(-50));
     }
 
     @Test
-    public void testContains() throws Exception {
+    public void testContainsWhileHasNotElement() throws Exception {
 
+        assertEquals("incorrect status of element", false, listSecond.contains(154));
     }
 
     @Test
-    public void testGet() throws Exception {
-
+    public void testContainsAfterAddElement() throws Exception {
+        listSecond.addElement(99);
+        assertEquals("incorrect status of element", true, listSecond.contains(99));
     }
 
     @Test
-    public void testSet() throws Exception {
+    public void testContainsAfterRemoveElement() throws Exception {
+        listSecond.removeElement(0);
+        assertEquals("incorrect status of element", false, listSecond.contains(0));
+    }
 
+    @Test
+    public void testGetWhileElementIsCorrect() throws Exception {
+        assertEquals("incorrect element of this index", -50, (int) listSecond.get(1));
+    }
+
+    @Test
+    public void testGetWhileIndexGreaterThanSize() throws Exception {
+        assertEquals("index is greater than size", null, listSecond.get(100));
+    }
+    @Test
+    public void testGetWhileIndexNegative() throws Exception {
+        assertEquals("index is negative", null,  listSecond.get(-1));
+    }
+    @Test
+    public void testGetAfterRemoveElementOfThisIndex() throws Exception {
+        listSecond.removeElement(2);
+        assertEquals("incorrect element of this index after remove", 85723, (int) listSecond.get(2));
+    }
+
+    @Test
+    public void testSetPositiveChange() throws Exception {
+        listSecond.set(0, -123);
+        assertEquals("incorrect element of index after set", -123 , (int) listSecond.get(0));
     }
 
 
 
-    }
+
+}
